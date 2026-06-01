af6896dc0f7a2047e952b011b5ef3c7c6573bc7f
Prashant Jatoo <jatooprashant099@gmail.com>
2026-06-01 13:12:14 +0400

    Task Update Diagnosis 1

--- PMS/server/src/Services/ProjectTask.Service.cs
+++ PMS/server/src/Services/ProjectTask.Service.cs
@@ -195,8 +195,8 @@ public class ProjectTaskService
             taskQueryExtension: t => t.Where(t => t.AssignedByID == userID)
         );
 
-        logger.LogInformation("New DueDate: {DueDate}, Previous DueDate: {PreviousDueDate}", dueDate, task.DueDate);
-        logger.LogInformation("DueDate Updated: {dueDateUpdated}", dueDate == task.DueDate);
+        logger.LogDebug("New DueDate: {DueDate}, Previous DueDate: {PreviousDueDate}", dueDate, task.DueDate);
+        logger.LogDebug("DueDate Updated: {dueDateUpdated}", dueDate == task.DueDate);
 
         bool dueDateUpdated = false;
         using var transaction = await dbContext.Database.BeginTransactionAsync();
